f7f75f34176f7ea3c43a8f401921b7aee0604d84
Luca Gouveia <38117857+luca-gouveia@users.noreply.github.com>
2023-12-17 16:30:05 -0300

    Ajuste de codigo

--- src/main/java/com/gouveia/imdb/controller/UsuarioController.java
+++ src/main/java/com/gouveia/imdb/controller/UsuarioController.java
@@ -1,17 +1,4 @@
 package com.gouveia.imdb.controller;
 
-import com.gouveia.imdb.enums.Genero;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/usuarios")
 public class UsuarioController {
-
-    @PostMapping
-    public ResponseEntity recuperarTodosFilmes() {
-        return ResponseEntity.ok(Genero.ACAO.getDescricao());
-    }
 }
---
+++ src/main/java/com/gouveia/imdb/dto/UsuarioDTO.java
@@ -0,0 +1,2 @@
+package com.gouveia.imdb.dto;public record UsuarioDTO() {
+}
