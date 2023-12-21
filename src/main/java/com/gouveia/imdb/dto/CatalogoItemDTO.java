e85b241e4de050fce7d20a424b0ba8cbb6c0fb04
Luca Gouveia <38117857+luca-gouveia@users.noreply.github.com>
2023-12-21 04:54:28 -0300

    ajusta para usuario logado nao se apagar e ajusta entrada dto de titulo

--- src/main/java/com/gouveia/imdb/dto/CatalogoItemDTO.java
+++ src/main/java/com/gouveia/imdb/dto/CatalogoItemDTO.java
@@ -2,5 +2,5 @@ package com.gouveia.imdb.dto;
 
 import java.util.List;
 
-public record CatalogoItemDTO(String titulo, List<String> genero, String diretor, String atores, String descricao, String imdbID) {
+public record CatalogoItemDTO(String titulo, List<String> genero, String diretor, String atores, String descricao, String imdbID, String linkImagem) {
 }
